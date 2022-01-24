a4db8e198cd04dcd3eb6e500397cd1cb055fedfb
EgoTkachenco <egortkachenco@gmail.com>
2022-01-24 11:45:51 +0200

    Remove articles population from categories response

--- api/category/controllers/category.js
+++ api/category/controllers/category.js
@@ -4,9 +4,9 @@ module.exports = {
   async find(ctx) {
     let entities;
     if (ctx.query._q) {
-      entities = await strapi.services.category.search(ctx.query);
+      entities = await strapi.services.category.search(ctx.query, []);
     } else {
-      entities = await strapi.services.category.find(ctx.query);
+      entities = await strapi.services.category.find(ctx.query, []);
     }
     for (let i = 0; i < entities.length; i++) {
       entities[i].articlesCount = await strapi.services.article.count({
